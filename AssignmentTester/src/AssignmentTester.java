46eedae9c06ea806a67d8366f9a3d904f691ece3
P3+J3^u! <dfischer@smu.edu>
2015-09-15 20:36:32 -0500

    Setting up repo.

---
+++ AssignmentTester/src/AssignmentTester.java
@@ -0,0 +1,163 @@
+import com.sun.javaws.exceptions.InvalidArgumentException;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.tools.JavaCompiler;
+import javax.tools.ToolProvider;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+
+//import org.junit.runner.JUnitCore;
+//import org.junit.runner.Result;
+
+/**
+ * Created by dfischer on 9/8/2015.
+ */
+public class AssignmentTester
+{
+   private static final String LookupTable = "TestsForAssignmentLookupTable.xml";
+
+   /**
+    * AssignmentTest can be run as a standalone program. If passed the name of a Java file (or files) as an argument,
+    * it will run whatever tests are appropriate for that file according to the accompanying
+    * TestsForAssignmentLookupTable.xml file.
+    *
+    * @param args
+    */
+   public static void main (String... args)
+   {
+      //This program only makes sense if there is at least one file to test
+      if (0 == args.length)
+      {
+         System.out.println("Usage: java AssignmentTest <fileToTest> OR " +
+               "java AssignmentTest <fileToTest> <fileToTest>...");
+         System.out.println("\ni.e. java AssignmentTest Hello.java");
+         System.out.println("or i.e. java AssignmentTest Hello.java Age.java SimpleCalculator.java Next.java");
+      }
+
+      System.out.println("Entering for loop");
+      // test all of the programs passed in via command line arguments
+      for (String argument : args)
+      {
+         try
+         {
+            System.out.println("Running test for " + argument);
+            runAllTestsFor(argument);
+         } catch (Exception ex)
+         {
+            System.out.println("Error: failed while attempting to test " + argument);
+            ex.printStackTrace();
+         }
+      }
+   }
+
+   /**
+    * Will run all of the JUnit tests corresponding to the input file. Determines which JUnit tests to run via lookup
+    * in the TestsForAssignmentLookupTable.xml file. Throws a bevy of exceptions associated with not being able to
+    * build a JUnitTest object from the class name given in the XML file.
+    *
+    * @param argument
+    */
+   public static void runAllTestsFor (String argument) throws NoSuchMethodException, IllegalAccessException,
+         InvocationTargetException, InstantiationException
+   {
+      try
+      {
+         // all Java source code files must have a .java suffix
+         if (!argument.endsWith(".java"))
+         {
+            String[] message = {"Incorrect file suffix; expected *.java, found: " + argument};
+            throw new InvalidArgumentException(message);
+         }
+
+         // verify that the program compiles
+         JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+         int result = compiler.run(null, null, null, argument);
+         System.out.println("Compile result code = " + result);
+
+         String[] className = argument.split("\\.java");
+
+         System.out.println("className[0] is " + className[0]);
+
+         // run the tests
+         Class<JUnitTest> testClass = getNameOfTestClassFor(argument);
+         JUnitTest test = testClass.getConstructor(JUnitTest.class).newInstance();
+//         JUnitCore junit = new JUnitCore();
+//         Result result = junit.run(test);
+      }
+      // At this point, regardless of the exception type the outcome is the same, so might as well just catch the
+      // superclass.
+      catch (Exception ex)
+      {
+         System.out.println("Error testing " + argument);
+         System.out.println(ex.toString());
+         ex.printStackTrace();
+      }
+   }
+
+   /**
+    * Uses the TestsForAssignmentLookupTable.xml file to determine which JUnitTest to run on the input file. This
+    * method expects each source code file to be tested by a single JUnitTest class in a 1:1 manner. Support for more
+    * is unnecessary at the time of this writing, and in any case would necessarily cause this method to abort or
+    * return only the first (as it returns a single Class<JUnitTest> object, not any kind of array or List.
+    *
+    * @param argument
+    * @return
+    */
+   private static Class<JUnitTest> getNameOfTestClassFor (String argument) throws TestNotFoundException
+   {
+      try
+      {
+         // all of these steps are really necessary to get the XML loaded and parsed
+         File testList = new File("res/" + LookupTable);
+         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         Document doc = dBuilder.parse(testList);
+         doc.getDocumentElement().normalize();
+         NodeList testableSourceCodeFiles = doc.getElementsByTagName("file_test_mapping");
+
+         // iterate over all of the testable files in the XML files
+         for (int i = 0; i < testableSourceCodeFiles.getLength(); i++)
+         {
+            Node curElement = testableSourceCodeFiles.item(i);
+
+            /*
+             * test each element in the file against our input argument until we find a match; this is a terrible,
+              * terrible method of finding the match, and I should look up the proper XML stuff API and fix it as
+              * soon as I can possibly get un-lazy.
+             */
+            if (curElement.getNodeType() == Node.ELEMENT_NODE)
+            {
+               Element element = (Element) curElement;
+               String fileName = element.getAttribute("source_code_file_name");
+
+               // ick
+               if (fileName.equals(argument))
+               {
+                  // this is such a horrifyingly fragile design even I don't understand why I don't bother to do better
+                  // oh wait yes I do it's because the XML Java API IS TERRIBLE and I haven't seen a betterer one.
+                  String testName = element.getChildNodes().item(1).getTextContent();
+
+                  // yay reflection
+                  Class<JUnitTest> testClass;
+                  testClass = (Class<JUnitTest>) Class.forName(testName);
+
+                  return testClass;
+               }
+            }
+         }
+      }
+      // lazily handling this for now. TODO: fixit.
+      catch (Exception e) { e.printStackTrace(); }
+      // if we got here then we failed to find the correct test, one way or another.
+      finally { throw new TestNotFoundException(); }
+   }
+
+   // TODO: make this descriptive
+   private static class TestNotFoundException extends ClassNotFoundException {}
+}
+
---
+++ AssignmentTester/src/GitSwitcher.java
@@ -0,0 +1,12 @@
+/**
+ * Created by dfischer on 9/8/2015.
+ */
+public class GitSwitcher
+{
+   public static void main (String... args)
+   {
+      System.out.println("Running GetSwitcher as a standalone. Testing...\n");
+
+      System.out.println("TODO: things");
+   }
+}
